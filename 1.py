9be06db4bd0017415d9f4d33d43d5b60b52ac27a
Zheka-png <evgeny232008@gmail.com>
2024-11-12 15:36:17 +0000

    ghfduv

---
+++ 1.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("Маша + Петя = Любовь")
+print("x = 3 + 4")
+print("x = 7")
+
+print("x = Hello World!")
+print("x = 3 + 4")
+print("x = 3 / 4")
+
+a = [1,5, "Good","Bad"]
+b = [9,"Blue","Red",11]
+print(a[1]+b[3])
